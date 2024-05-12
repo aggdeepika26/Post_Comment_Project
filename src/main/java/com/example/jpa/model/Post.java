a7dbadb5181890248b99afe1730379abfcb8580b
adeepika26@yahoo.in <adeepika26@yahoo.in>
2024-05-12 22:45:37 +0200

    post-comment-project

---
+++ src/main/java/com/example/jpa/model/Post.java
@@ -0,0 +1,89 @@
+package com.example.jpa.model;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.*;
+@Entity
+@Table(name = "posts")
+public class Post extends AuditModel
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    @Size(max = 100)
+    @Column(unique = true)
+    private String title;
+    @NotNull
+    @Size(max = 250)
+    private String description;
+    @NotNull
+    @Lob
+    private String content;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "post")
+    private Set<Comment> comments = new HashSet<>();
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Set<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(Set<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public Post(Long id, String title, String description, String content, Set<Comment> comments) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.content = content;
+        this.comments = comments;
+    }
+
+    public Post() {
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", content='" + content + '\'' +
+                ", comments=" + comments +
+                '}';
+    }
+}
---
+++ src/test/java/jpaonetomanydemo/JpaOneToManyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package jpaonetomanydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaOneToManyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
